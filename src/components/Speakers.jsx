c6fc2ff6ecb6d0bdf382481a53d68bce7f11cabe
Lofty_Brambles <prantik.ts@gmail.com>
2025-08-09 21:15:52 +0000

    chore: install and run prettier

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import Card from './Cards'
+import React from "react";
+import Card from "./Cards";
 
 function Speakers() {
   const speakers = [
@@ -7,43 +7,43 @@ function Speakers() {
       image: "/don.webp",
       name: "Krishna",
       designation: "Chief in Charge",
-      linkedin: "https://www.google.com"
+      linkedin: "https://www.google.com",
     },
     {
       image: "/don.webp",
       name: "Krishna",
       designation: "Chief in Charge",
-      linkedin: "https://www.google.com"
+      linkedin: "https://www.google.com",
     },
     {
       image: "/don.webp",
       name: "Krishna",
       designation: "Chief in Charge",
-      linkedin: "https://www.google.com"
+      linkedin: "https://www.google.com",
     },
-    
+
     // Add more speakers as needed...
   ];
 
   return (
     <div>
-      <h1 className='w-full text-center text-6xl font-extrabold mt-3'>SPEAKERS</h1>
+      <h1 className="w-full text-center text-6xl font-extrabold mt-3">SPEAKERS</h1>
 
-    <div className="flex justify-center items-center min-h-screen w-full p-4  box-border">
-      <div className="grid gap-4 w-full max-w-7xl sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-        {speakers.map((speaker, idx) => (
-          <Card
-          key={idx}
-          image={speaker.image}
-          name={speaker.name}
-          designation={speaker.designation}
-          linkedin={speaker.linkedin}
-          />
-        ))}
+      <div className="flex justify-center items-center min-h-screen w-full p-4  box-border">
+        <div className="grid gap-4 w-full max-w-7xl sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+          {speakers.map((speaker, idx) => (
+            <Card
+              key={idx}
+              image={speaker.image}
+              name={speaker.name}
+              designation={speaker.designation}
+              linkedin={speaker.linkedin}
+            />
+          ))}
+        </div>
       </div>
     </div>
-</div>
-  )
+  );
 }
 
-export default Speakers
+export default Speakers;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Timeline() {
-  return (
-    <div>Timeline</div>
-  )
+  return <div>Timeline</div>;
 }
 
-export default Timeline
+export default Timeline;
